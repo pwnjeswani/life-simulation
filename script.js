806b961d6a555f3d2caaadde67fb85480835405c
pwnjeswani <pwnjeswani@gmail.com>
2024-08-22 23:21:41 +0530

    Validation of input fields

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ let back = document.querySelector('.back-btn');
 
 simulate.addEventListener('click', (e) => {
     e.preventDefault();
-    const floors = parseInt(document.getElementById("floors").value);
-    const lifts = parseInt(document.getElementById("lifts").value);
+    const floors = document.querySelector('#floors').value;
+    const lifts = document.querySelector('#lifts').value;
+
+     console.log(floors, "-- ",lifts);
     
+     if (isNaN(floors) || floors < 1 || floors > 50) {
+        alert('Please enter a valid number of floors between 1 and 50.');
+        return;
+      }
+    
+      if (isNaN(lifts) || lifts < 2 || lifts > 50) {
+        alert('Please enter a valid number of lifts between 2 and 50.');
+        return;
+      }
+
     document.querySelector('.lifts-section').style.display = 'block';
     document.querySelector('.input-section').style.display = 'none';
 });
